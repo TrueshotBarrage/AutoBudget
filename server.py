ed43fd696b7ff300f39cb36705938563de0e233f
Dave Kim <davidkim2106@gmail.com>
2025-07-22 18:19:30 -0400

    Added logging for server script

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
 from datetime import datetime
+import logging
+
 from flask import Flask
 
 from gmail_client import main as abg_pipeline
 import utils.fs as fs
+
+logging.basicConfig(
+    level=logging.INFO, format="%(levelname)-9s %(asctime)s - [%(name)s] %(message)s"
+)
+logger = logging.getLogger("AutoBudget Server")
 
 app = Flask(__name__)
 
@@ -16,9 +23,12 @@ def root():
 @app.route("/run")
 def run_gmail_client():
     # fs._initialize_env_vars_into_files()
+    logger.info("Initializing env vars...")
     env_vars = fs.init_env_vars()
 
+    logger.info("Executing AutoBudget pipeline...")
     logs = abg_pipeline(env_vars)
+    logger.info("AutoBudget pipeline successfully executed. Now logging the output artifacts...")
 
     log_out = ""
     with open(f"log_{datetime.now().strftime('%m_%d_%Y_%H_%M')}.txt", "w") as f:
@@ -26,7 +36,10 @@ def run_gmail_client():
             f.write(f"{line}\n")
             log_out += f"{line}\n"
 
-    return f"AutoBudget Pipeline Run Successful.\n\n{log_out}"
+    success_msg = f"AutoBudget Pipeline Run Successful.\n\n{log_out}"
+    logger.info(success_msg)
+
+    return success_msg
 
 
 if __name__ == "__main__":
